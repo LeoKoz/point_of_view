35566380a941e57de512353828def91dcc28d410
Leo Koz <LeonidPr.17@gmail.com>
2011-10-21 16:23:15 +0200

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PointOfView::Application.config.secret_token = 'a26b792515739f6cabf4c4435940b8225e3d8c11670c383839f2dbfe54286dd8e5df2ae7accb2b60107624dadc94b7fbad98a1b793277f4205bcf64c310e17b1'
